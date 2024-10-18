44b5d9e588da64db6c0de9bd3b5b6ed9aeaaba0c
wilitongomes <wiliton.ito@gmail.com>
2024-10-17 22:30:47 -0300

    tabuleiro

--- Jogo_da_velha.py
+++ Jogo_da_velha.py
@@ -3,12 +3,16 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((500, 500))
+screem = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('Jogo da velha')
 clock = pygame.time.Clock()
+
+fonte_quadrinhos = pygame.font.SysFont('Comic Sams ms', 100, True, True)
 running = True
-cor_fundo = 1 #azul
-#cor_fundo = 2 #vermelho
+
+personagem_x = fonte_quadrinhos.render('X', True, 'red')
+personagem_y = fonte_quadrinhos.render('O', True, 'blue')
+cor_fundo = 1 
 
 while running:
     # poll for events
@@ -21,14 +25,17 @@ while running:
             cor_fundo = cor_fundo + 1
             if(cor_fundo >3):
                 cor_fundo = 1
+        
+        pygame.draw.line(screem, 'white', (175, 25), (175, 475), 10)
+        pygame.draw.line(screem, 'white', (325, 25), (325, 475), 10)
+        pygame.draw.line(screem, 'white', (25, 175), (475, 175), 10)
+        pygame.draw.line(screem, 'white', (25, 325), (475, 325), 10)
 
-    if cor_fundo == 1:
-        screen.fill('blue')
-    elif cor_fundo == 2:
-        screen.fill('red')
-    else:
-        screen.fill('purple')
-    # flip() the display to put your work on screen
+        screem.blit(personagem_x,(50,60))
+        screem.blit(personagem_y,(210,60))
+        screem.blit(personagem_y,(400,60))
+        
+    # flip() the display to put your work on screem
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
