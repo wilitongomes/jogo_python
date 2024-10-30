0d814dcca0d9591e5727c09a91c432633a414b4f
wilitongomes <wiliton.ito@gmail.com>
2024-10-29 21:58:09 -0300

    correcoa do loop

--- Jogo_da_velha.py
+++ Jogo_da_velha.py
@@ -10,12 +10,45 @@ clock = pygame.time.Clock()
 fonte_quadrinhos = pygame.font.SysFont('Comic Sams ms', 100, True, True)
 running = True
 
-personagem_x  = fonte_quadrinhos.render('x ', True, 'red')
-personagem_y = fonte_quadrinhos.render('O', True, 'blue')
-apresenta_personagem = 0 
-x = 0
-y = 0
+personagem_x = fonte_quadrinhos.render('x', True, 'red')
+personagem_o = fonte_quadrinhos.render('O', True, 'blue')
 
+jogador_atual = personagem_x
+rodadas = 0
+tabuleiro_desenhado = False
+
+coordenada_x = 0
+coordenada_y = 0
+
+def desenha_tabuleiro(espessura, cor):
+    #                                   Origem      Destino
+    #                                   (x  , Y )    (x  , y) 
+    pygame.draw.line(screem, cor, (175, 25), (175, 475), espessura)
+    pygame.draw.line(screem, cor, (325, 25), (325, 475), espessura)
+    pygame.draw.line(screem, cor, (25, 175), (475, 175), espessura)
+    pygame.draw.line(screem, cor, (25, 325), (475, 325), espessura)
+def faz_jogada():    
+     #Primeira Linha
+        if coordenada_x > 0 and coordenada_x < 200 and coordenada_y < 200:
+            screem.blit(jogador_atual,(60,60)) #primeiro
+        elif coordenada_x >= 200 and coordenada_x < 400 and coordenada_y < 200:    
+            screem.blit(jogador_atual,(215,60)) #segundo
+        elif coordenada_x >= 400 and coordenada_y < 200:
+            screem.blit(jogador_atual,(380,60)) #terceiro
+        #Segunda Linha
+        elif coordenada_x < 200 and coordenada_y >= 200 and coordenada_y < 400:
+            screem.blit(jogador_atual,(60,220)) #Quarta
+        elif coordenada_x >= 200 and coordenada_x < 400 and coordenada_y >= 200 and coordenada_y <400:  
+            screem.blit(jogador_atual,(215,215)) #Quinta
+        elif coordenada_x >= 400 and coordenada_y >= 200 and coordenada_y < 400:
+             screem.blit(jogador_atual,(380,210)) #Sexto
+        #Terceira Linha
+        elif coordenada_x < 200 and coordenada_y >= 400:
+            screem.blit(jogador_atual,(60,380)) #Setimo
+        elif coordenada_x >= 200 and coordenada_x < 400 and coordenada_y >= 400:
+            screem.blit(jogador_atual,(215,380)) #Oitavo
+        elif coordenada_x >= 400 and coordenada_y >= 400:  
+            screem.blit(jogador_atual,(380,380)) #Nono        
 while running:
     # poll for events
     # pygame.QUIT event means the user apresenta_personagemed x  to close your window
@@ -25,42 +58,32 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             print('clicou')
             click_pos = pygame.mouse.get_pos()
-            print('eixo x', click_pos[0])
-            print('eixo Y', click_pos[1])
-            x = click_pos[0]
-            y = click_pos[1]
-            apresenta_personagem = apresenta_personagem + 1
-            if(apresenta_personagem >=10):
+            print('coordenada_x', click_pos[0])
+            print('coordenada_y', click_pos[1])
+            coordenada_x = click_pos[0]
+            coordenada_y = click_pos[1]
+            rodadas = rodadas + 1
+            if(rodadas >= 10):
                 screem.fill('black')
-                apresenta_personagem = 0
-        #                                   Origem      Destino
-        #                                   (x  , Y )    (x  , y) 
-        pygame.draw.line(screem, 'white', (175, 25), (175, 475), 10)
-        pygame.draw.line(screem, 'white', (325, 25), (325, 475), 10)
-        pygame.draw.line(screem, 'white', (25, 175), (475, 175), 10)
-        pygame.draw.line(screem, 'white', (25, 325), (475, 325), 10)
+                rodadas = 0
+                coordenada_x = 0
+                coordenada_Y = 0
+                tabuleiro_desenhado = False
 
-        #Primeira Linha
-        if x > 0 and x < 200 and y < 200:
-            screem.blit(personagem_x ,(60,60)) #primeiro
-        elif x >= 200 and x < 400 and y < 200:    
-            screem.blit(personagem_y,(215,60)) #segundo
-        elif x >= 400 and y < 200:
-            screem.blit(personagem_y,(380,60)) #terceiro
-        #Segunda Linha
-        elif x < 200 and y>= 200 and y< 400:
-            screem.blit(personagem_x ,(60,220)) #Quarta
-        elif x >= 200 and x < 400 and y>= 200 and y<400:  
-            screem.blit(personagem_y,(215,215)) #Quinta
-        elif x >= 400 and y>= 200 and y< 400:
-             screem.blit(personagem_y,(380,210)) #Sexto
-        #Terceira Linha
-        elif x < 200 and y>= 400:
-            screem.blit(personagem_x ,(60,380)) #Setimo
-        elif x >= 200 and x < 400 and y>= 400:
-            screem.blit(personagem_y,(215,380)) #Oitavo
-        elif x >= 400 and y>= 400:  
-            screem.blit(personagem_y,(380,380)) #Nono        
+            if rodadas != 1:
+                if jogador_atual == personagem_x:
+                   jogador_atual = personagem_o
+                else:
+                   jogador_atual = personagem_x          
+            else:
+                   jogador_atual = personagem_x
+            faz_jogada()          
+    
+    if tabuleiro_desenhado == False:     
+        desenha_tabuleiro(10, 'blue')
+        tabuleiro_desenhado = True
+   
+       
     # flip() the display to put your work on screem
     pygame.display.flip()
 
